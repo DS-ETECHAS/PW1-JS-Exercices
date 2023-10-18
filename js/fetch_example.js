476a6b46e479cd43225bccc3f9bb35fc2c89187b
Guilherme Santos <guilherme.santos536@etec.sp.gov.br>
2023-10-18 15:46:02 -0300

    feat: Exemplos de fetch realizados em aula

--- js/fetch_example.js
+++ js/fetch_example.js
@@ -1,8 +1,44 @@
 function fetchApiData(){
    console.log("Fetching...")
    let result = []
-   const data = fetch('https://api.magicthegathering.io/v1/cards').then( data => {return data.json()}).then(json => {
+   const data = fetch('https://api.magicthegathering.io/v1/cards').then( response => {return response.json()}).then(json => {
    result = json.cards
-   result.map( carta => { console.log(carta.name) })
+   result.map( carta => { console.log(carta.name)
+   console.log(carta.imageUrl) 
+   console.log(carta.artist)})
    })
+}
+
+
+//Exemplo feito em aula
+//Quero pegar os dados que vem de uma API
+const URL = 'https://jsonplaceholder.typicode.com/posts'
+const MTGURL = 'https://api.magicthegathering.io/v1/'
+
+
+function getApiData(){
+//Busca os dados da API
+//  GET  URL ( "Link da URL" /  ENDPOINT) 
+
+//Jeito mais bonitinho de se fazer
+//fetch(`${URL}/posts`)
+
+//Assíncrono
+//Esse código é executado de maneira assíncrona.
+//Eu "Peço" algo pra API e "Espero a resposta"
+const resultado = fetch(URL).then(
+    response => {
+        console.log(response)
+    }
+    )
+}
+
+function getCardsList(){
+    //fetch(`${MTGURL} + /cards `)
+    fetch(MTGURL + '/cards').then( response => {console.log(response.json())})
+}
+
+function getCard(id){
+    //fetch(`${MTGURL} + /cards `)
+    fetch(MTGURL + '/cards/' + id).then( response => {console.log(response.json())})
 }
